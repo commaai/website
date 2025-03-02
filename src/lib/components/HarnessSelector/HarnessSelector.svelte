712883d63afb0a42d0cd21671912c29180037168
David <dt052901@gmail.com>
2025-03-01 23:03:49 -0600

    fix: allow clear button to clear search input instead of closing dropdown

--- src/lib/components/HarnessSelector/HarnessSelector.svelte
+++ src/lib/components/HarnessSelector/HarnessSelector.svelte
@@ -62,6 +62,7 @@
 
   const handleClear = () => {
     // clear search input
+    const clearedInput = inputValue !== "";
     inputValue = "";
     handleInput();
     inputRef?.focus();
@@ -69,8 +70,10 @@
     selection = null;
     onChange(null);
     updateQueryParams(null);
-    // also close the dropdown if it's open
-    menuOpen = false;
+    // close the dropdown if it's open and we didn't clear the search input
+    if (menuOpen && !clearedInput) {
+      menuOpen = false;
+    }
   }
 
   /* Dropdown Options */
