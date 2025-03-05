d6e82507590c63e374eaa707c0b370be6b6bc6db
Shane Smiskol <shane@smiskol.com>
2025-03-04 22:27:42 -0800

    rename

--- scripts/compatibility.py
+++ scripts/compatibility.py
@@ -4,11 +4,11 @@ from opendbc.car.docs import generate_cars_md, get_all_car_docs
 
 WEB4_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
 VEHICLES_OUT_FILE = os.path.join(WEB4_DIR, "src/lib/vehicles.json")
-HARNESS_PARTS_OUT_FILE = os.path.join(WEB4_DIR, "static/harness_parts.html")
+HARNESS_PARTS_OUT_FILE = os.path.join(WEB4_DIR, "static/harness-parts.html")
 
 if __name__ == '__main__':
   for out_file, template_file in ((VEHICLES_OUT_FILE, "templates/vehicles_template.json"),
-                                  (HARNESS_PARTS_OUT_FILE, "templates/harness_parts_template.html")):
+                                  (HARNESS_PARTS_OUT_FILE, "templates/harness-parts_template.html")):
     content = generate_cars_md(get_all_car_docs(), os.path.join(WEB4_DIR, template_file))
     with open(out_file, "w") as f:
       f.write(content)
