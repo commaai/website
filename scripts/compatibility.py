85a83d93fe8a4e919bfef0e04e100c040ff726f0
Shane Smiskol <shane@smiskol.com>
2025-03-04 18:41:44 -0800

    stash

--- scripts/compatibility.py
+++ scripts/compatibility.py
@@ -4,7 +4,7 @@ from opendbc.car.docs import generate_cars_md, get_all_car_docs
 
 WEB4_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
 VEHICLES_OUT_FILE = os.path.join(WEB4_DIR, "src/lib/vehicles.json")
-HARNESS_PARTS_OUT_FILE = os.path.join(WEB4_DIR, "src/lib/vehicles.html")
+HARNESS_PARTS_OUT_FILE = os.path.join(WEB4_DIR, "static/harness_parts.html")
 
 if __name__ == '__main__':
   for out_file, template_file in ((VEHICLES_OUT_FILE, "templates/vehicles_template.json"),
