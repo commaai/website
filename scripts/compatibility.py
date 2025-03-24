5abc482cea955c8c2972b64d6b61c02c0eb8dc88
Cameron Clough <cameronjclough@gmail.com>
2025-03-24 17:01:48 +0000

    fixes - add OBD-II and use harness connector name when group is single harness

--- scripts/compatibility.py
+++ scripts/compatibility.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 import os
 import jinja2
+import json
 from opendbc.car.docs import get_all_car_docs, get_all_footnotes, group_by_make
 from opendbc.car.docs_definitions import BaseCarHarness, CarDocs, CarHarness, Column, Device, ExtraCarsColumn, PartType, SupportType
 
 WEB4_DIR = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)), ".."))
+CAR_HARNESSES_JSON = os.path.join(WEB4_DIR, "src/lib/constants/car-harnesses.json")
+
+TEMPLATES = (("vehicles_template.json", "src/lib/vehicles.json"),
+             ("harness-parts_template.html", "static/harness-parts.html"),
+             ("non-standard-harness-parts_template.html", "src/lib/non-standard-harness-parts.html"),
+             ("standard-harness-parts_template.html", "src/lib/standard-harness-parts.html"))
 
 def generate_cars_md(all_car_docs: list[CarDocs], template_fn: str) -> str:
   with open(template_fn) as f:
@@ -15,21 +22,32 @@ def generate_cars_md(all_car_docs: list[CarDocs], template_fn: str) -> str:
   base_harness_parts = frozenset({ part.value.name for part in BaseCarHarness("").parts })
   standard_harness_parts = [*base_harness_parts, "harness connector"]
 
+  with open(CAR_HARNESSES_JSON) as f:
+    store_car_harness_names = [product["title"] for product in json.load(f)]
+
+  # group harnesses with identical part lists
   non_standard_harness_parts = {}
-  for car in all_car_docs:
-    harness_connector = next((part for part in car.car_parts.parts if part.part_type == PartType.connector), None)
-    if not harness_connector: continue
-    parts = frozenset({ part.value.name for part in harness_connector.value.parts })
+  for harness in CarHarness:
+    if harness.value.name.replace(" connector", "") not in store_car_harness_names:
+      print("skipping not in store", harness)
+      continue
+    parts = frozenset({ part.value.name for part in harness.value.parts })
     if base_harness_parts == parts: continue
-    if harness_connector.value.has_connector:
+    if harness.value.has_connector:
       parts = frozenset({ *parts, "harness connector" })
     if parts not in non_standard_harness_parts:
       non_standard_harness_parts[parts] = set()
-    non_standard_harness_parts[parts].add(harness_connector.value.name.replace(" connector", ""))
+    non_standard_harness_parts[parts].add(harness)
 
   non_standard_harness_parts_by_name = {}
-  for parts, harness_names in non_standard_harness_parts.items():
-    harness_names = sorted(harness_names, reverse=True)
+  for parts, harnesses in non_standard_harness_parts.items():
+    # use actual connector name when this group is a single harness
+    if len(harnesses) == 1:
+      for harness in harnesses: break
+      parts = [part for part in parts if part != "harness connector"] + [harness.value.name]
+
+    # build group title
+    harness_names = sorted((harness.value.name.replace(" connector", "") for harness in harnesses), reverse=True)
     name = harness_names.pop()
     while len(harness_names) > 1:
       name = "{}, {}".format(name, harness_names.pop())
@@ -47,10 +65,7 @@ def generate_cars_md(all_car_docs: list[CarDocs], template_fn: str) -> str:
                          BaseCarHarness=BaseCarHarness, upstream_car_docs=upstream_car_docs)
 
 if __name__ == '__main__':
-  for template_file, out_file in (("vehicles_template.json", "src/lib/vehicles.json"),
-                                  ("harness-parts_template.html", "static/harness-parts.html"),
-                                  ("non-standard-harness-parts_template.html", "src/lib/non-standard-harness-parts.html"),
-                                  ("standard-harness-parts_template.html", "src/lib/standard-harness-parts.html")):
+  for template_file, out_file in TEMPLATES:
     content = generate_cars_md(get_all_car_docs(), os.path.join(WEB4_DIR, "templates", template_file))
     with open(os.path.join(WEB4_DIR, out_file), "w") as f:
       f.write(content)
