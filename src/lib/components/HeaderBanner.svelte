f8f8a75ba3c32f92a9b5debd75373a62eceafcd1
Shane Smiskol <shane@smiskol.com>
2025-08-25 14:01:59 -0700

    wouldn't normally but looks better here

--- src/lib/components/HeaderBanner.svelte
+++ src/lib/components/HeaderBanner.svelte
@@ -1,7 +1,7 @@
 <a href="/jobs.html" target="_blank" class="banner">
   <div class="wrapper">
     <strong class="headline">
-      FLASH SALE: take 10% off comma 3X for openpilot 0.10!
+      FLASH SALE: Take 10% off comma 3X for openpilot 0.10!
     </strong>
   </div>
 </a>
