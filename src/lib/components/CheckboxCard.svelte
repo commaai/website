341c9af76dc6996228f1912383ce9e7b190c8b80
Shane Smiskol <shane@smiskol.com>
2025-11-11 16:50:10 -0800

    check box card

---
+++ src/lib/components/CheckboxCard.svelte
@@ -0,0 +1,28 @@
+<script>
+  import NoteCard from "./NoteCard.svelte";
+
+  export let title;
+  export let checked = false;
+  export let onToggle;
+</script>
+
+<label class="checkbox-card">
+  <NoteCard {title}>
+    <input type="checkbox" slot="icon" checked={checked} on:change={() => onToggle && onToggle()} />
+    <slot></slot>
+  </NoteCard>
+</label>
+
+<style>
+  .checkbox-card {
+    cursor: pointer;
+    display: block;
+  }
+
+  .checkbox-card :global(input[type="checkbox"]) {
+    width: 1.25rem;
+    height: 1.25rem;
+    cursor: pointer;
+  }
+</style>
+
