1cd9974e3bf22f1c1c85da9db01556585b4067c2
Shane Smiskol <shane@smiskol.com>
2026-08-14 18:47:07 -0700

    Revert "remove banner"
    This reverts commit 502917d12fda5ea61d1f2c5d2bbedfe909d1c38c.

---
+++ src/lib/components/HeaderBanner.svelte
@@ -0,0 +1,54 @@
+<div class="banner">
+  <div class="wrapper">
+    <a href="/shop/chestnut" class="headline">
+      <strong>Big GPU, big models.</strong>
+      <span>Meet chestnut ⟶</span>
+    </a>
+  </div>
+</div>
+
+<style>
+  .banner {
+    color: black;
+    position: relative;
+    z-index: 4;
+    background-color: var(--color-card-background-hover);
+  }
+
+  .wrapper {
+    padding: 1rem 0;
+    margin-left: auto;
+    margin-right: auto;
+    width: 85%;
+    max-width: 90rem;
+    font-size: 0.875rem;
+    line-height: 1.2;
+    text-align: center;
+
+    display: flex;
+    flex-direction: column;
+    grid-row-gap: 0.5rem;
+  }
+
+  .headline {
+    color: black;
+    font-family: Inter, sans-serif;
+    font-size: 1.25rem;
+    margin: 0 0.5rem;
+    text-decoration: none;
+  }
+
+  .headline span {
+    color: black;
+    margin-left: 0.35em;
+    font-weight: 700;
+    text-decoration: underline;
+    text-underline-offset: 0.15em;
+    white-space: nowrap;
+  }
+
+  .headline:hover span,
+  .headline:focus-visible span {
+    text-decoration-thickness: 2px;
+  }
+</style>
