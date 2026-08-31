f82b21edde603e55e094c3c3f51a42493ba6017e
stefpi <19478336+stefpi@users.noreply.github.com>
2026-08-31 11:34:44 -0700

    disable trade in when referral code is added

--- src/lib/components/ProductDescriptions/CommaFour.svelte
+++ src/lib/components/ProductDescriptions/CommaFour.svelte
@@ -183,9 +183,13 @@
       </NoteCard>
     {/if}
     <CheckboxCard bind:this={checkboxCardRef} title="${FOUR_TRADE_IN_CREDIT} credit with trade-in" checked={tradeInChecked} onToggle={handleTradeInToggle}
-                  disabled={disableBuyButtonText !== null}>
-      Get ${FOUR_TRADE_IN_CREDIT} credit when you trade in your old comma device. Any comma device, in any condition.
-      <a href="/shop/comma-four-trade-in">Instructions and Terms</a>
+                  disabled={disableBuyButtonText !== null || referralCode}>
+      {#if referralCode}
+        Trade in credit is not available when using a referral code.
+      {:else}
+        Get ${FOUR_TRADE_IN_CREDIT} credit when you trade in your old comma device. Any comma device, in any condition.
+        <a href="/shop/comma-four-trade-in">Instructions and Terms</a>
+      {/if}
     </CheckboxCard>
   </span>
 
